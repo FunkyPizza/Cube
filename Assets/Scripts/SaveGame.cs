7a416be1cf065e332d4d3c7e05de5dc99a8f9e19
FunkyPizza <FunkyPizza@users.noreply.github.com>
2019-02-12 09:47:53 +0000

    Settings UI update
    - Added settings UI

---
+++ Assets/Scripts/SaveGame.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SaveGame : MonoBehaviour {
+
+    [Header("SettingsButtons")]
+    [SerializeField]
+    GameObject SoundButton; //Used to swap sprite on press
+    [SerializeField]
+    GameObject VibesButton; //Used to swap sprite on press
+
+    [Header("SettingsSprites")]
+    [SerializeField]
+    Sprite SoundOn;
+    [SerializeField]
+    Sprite SoundOff;
+    [SerializeField]
+    Sprite VibesOn;
+    [SerializeField]
+    Sprite VibesOff;
+
+    bool isSoundOn;
+    bool isVibesOn;
+
+    
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+
+    public void ToggleSound()
+    {
+        if (isSoundOn)
+        {
+            isSoundOn = false;
+            SoundButton.GetComponent<Image>().sprite = SoundOff;
+        }
+        else
+        {
+            isSoundOn = true;
+            SoundButton.GetComponent<Image>().sprite = SoundOn;
+        }
+    }
+
+    public void ToggleVibes()
+    {
+        if (isVibesOn)
+        {
+            isVibesOn = false;
+            VibesButton.GetComponent<Image>().sprite = VibesOff;
+        }
+        else
+        {
+            isVibesOn = true;
+            VibesButton.GetComponent<Image>().sprite = VibesOn;
+        }
+    }
+}
